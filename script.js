401558a3e02309a578b2b6f9fe171841db5924e7
Ton1sss <145768686+Ton1sss@users.noreply.github.com>
2025-08-28 11:10:37 -0300

    jgkfçejag

---
+++ script.js
@@ -0,0 +1,6 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaresultado = document.querySelector(".caixa-resultado);
+const textoResultado = document.querySelector(".texto-resultado");
+
